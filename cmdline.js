464e50cc45090e77bc6e4f997fa0a3afd0e094e7
Greg Wiley <aztec.rex@jammm.com>
2016-12-27 11:36:10 -0800

    fix cmdline.json

--- cmdline.js
+++ cmdline.js
@@ -3,7 +3,7 @@ default: {
     clientId: clientIdDefault,
     privateKey: 'private.pem.key',
     clientCert: 'certificate.pem.crt',
-    caCert: 'root-CA.crt,
+    caCert: 'root-CA.crt',
     testMode: 1,
     reconnectPeriod: 3 * 1000,     /* milliseconds */
     delay: 4 * 1000                        /* milliseconds */
