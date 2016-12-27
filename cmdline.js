be63af4c21c0f6bc3ab243770a8ca22906171d0f
Greg Wiley <aztec.rex@jammm.com>
2016-12-27 06:48:50 +0000

    configuration

---
+++ cmdline.js
@@ -0,0 +1,10 @@
+default: {
+    region: 'us-east-1',
+    clientId: clientIdDefault,
+    privateKey: 'private.pem.key',
+    clientCert: 'certificate.pem.crt',
+    caCert: 'root-CA.crt,
+    testMode: 1,
+    reconnectPeriod: 3 * 1000,     /* milliseconds */
+    delay: 4 * 1000                        /* milliseconds */
+};
