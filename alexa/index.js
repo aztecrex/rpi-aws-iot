855d34622750629e821c29664b05de80a287de26
Greg Wiley <aztec.rex@jammm.com>
2016-12-27 21:05:15 -0800

    checkpoint

---
+++ alexa/index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const Alexa = require('alexa-sdk');
+
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+
+const languageStrings = {
+    'en-US': {
+        translation: {
+            FACTS: [
+                'A year on Mercury is just 88 days long.',
+                'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
+                'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
+                'On Mars, the Sun appears about half the size as it does on Earth.',
+                'Earth is the only planet not named after a god.',
+                'Jupiter has the shortest day of all the planets.',
+                'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
+                'The Sun contains 99.86% of the mass in the Solar System.',
+                'The Sun is an almost perfect sphere.',
+                'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
+                'Saturn radiates two and a half times more energy into space than it receives from the sun.',
+                'The temperature inside the Sun can reach 15 million degrees Celsius.',
+                'The Moon is moving approximately 3.8 cm away from our planet every year.',
+            ],
+            SKILL_NAME: 'Pi Thing',
+            ON_MESSAGE: "Light is lit",
+            OFF_MESSAGE: "Light is off",
+            FLASH_MESSAGE: "Whoop! Whoop!",
+            HELP_MESSAGE: 'You can say "turn on the light" or "flash the light", or, you can say "exit"... What can I help you with?',
+            HELP_REPROMPT: 'What can I help you with?',
+            STOP_MESSAGE: 'Goodbye!',
+        },
+    },
+};
+
+const handlers = {
+    'FlashIntent': function () {
+        this.emit(':tell', this.t('FLASH_MESSAGE'));
+    },
+    'OnIntent': function () {
+        this.emit(':tell', this.t('ON_MESSAGE'));
+    },
+    'OffIntent': function () {
+        this.emit(':tell', this.t('OFF_MESSAGE'));
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = this.t('HELP_MESSAGE');
+        const reprompt = this.t('HELP_MESSAGE');
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'SessionEndedRequest': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+};
+
+exports.handler = (event, context) => {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
